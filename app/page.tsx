06c30418602334caaa3ad2d166b2184ac2cc9730
socar-foden <foden@socar.kr>
2024-07-02 20:13:51 +0900

    chore/default-setting prettier 세팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <main>
-      <div>hello</div>
-    </main>
-  );
+    return (
+        <main>
+            <div>hello</div>
+        </main>
+    )
 }
